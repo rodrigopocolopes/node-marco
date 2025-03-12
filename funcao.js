a1d37c106aa9be0cb21412b5aa13cfda66fed8a2
Rodrigo Lopes <rodrigolopes.94@hotmail.com>
2025-03-11 21:13:43 -0300

    funções

---
+++ funcao.js
@@ -0,0 +1,47 @@
+// Entrada -> Processamento -> Saída
+
+let n1 = 2;
+let n2 = 3;
+let n3 = 4;
+
+function soma(n1, n2) {
+    let n3 = 5;
+    let resultado = n1 + n2 + n3;
+    return resultado;
+}
+
+
+function sub(n1, n2) {
+    let resultado = n1 - n2;
+    return resultado;
+}
+
+let res = sub(3, 5);
+
+/* console.log(res); */
+
+// função anonima
+
+let mult = function (n1, n2) {
+    return n1 * n2
+}
+
+/* console.log(mult(3, 4)) */
+
+/**
+ * Executa operação matemática
+ * @param {number} n1 primeiro numero
+ * @param {number} n2 segundo numero
+ * @param {Function} op operador
+ * @returns {Number}
+ */
+
+
+function calc(n1, n2, op) {
+    return op(n1, n2)
+}
+
+console.log(calc(3,5,mult));
+console.log(calc(3,5,soma));
+console.log(calc(3,5,sub));
+

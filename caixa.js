7e1986a22671914516b86276ac617136f9a2c821
Rodrigo Lopes <rodrigolopes.94@hotmail.com>
2025-03-14 20:13:32 -0300

    caixacomments

--- caixa.js
+++ caixa.js
@@ -9,28 +9,34 @@ process.stdin.on("data", function (valor) {
     let dez = quant.charAt(1);
     let un = quant.charAt(2);
 
+    // se 0 acusa erro
     if (quant == 0) {
         console.log("Digite um valor maior do que 0")
     }
 
+    // se maior que 600 acusa erro
     if (quant > 600) {
         console.log("O valor não pode ser maior do que 600")
     } else {
 
+        // se menor que 100 e maior que 10, muda a variável das casas
         if (quant < 100 && quant >= 10) {
             dez = quant.charAt(0);
             un = quant.charAt(1);
         }
 
+        // se menor que 10, muda a variável das casas de novo
         if (quant < 10 && quant > 0) {
             dez = 0;
             un = quant.charAt(0);
         }
 
+        // quantas notas de cem
         if (quant >= 100) {
             console.log(cent + " notas de cem")
-
         }
+
+        // quantas nodas de 50 e 10
         if (dez > 5) {
             console.log("Uma nota de 50");
             let deze = dez - 5;
@@ -41,6 +47,7 @@ process.stdin.on("data", function (valor) {
             console.log(dez + " nota(s) de dez");
         }
 
+        //quantas notas de 5 e 1
         if (un > 5) {
             console.log("Uma nota de 5");
             let uni = un - 5;

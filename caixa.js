8688ddc8331679bbc8544fa9cf9da6ee4989c457
Rodrigo Lopes <rodrigolopes.94@hotmail.com>
2025-03-14 19:35:12 -0300

    caixavalido

--- caixa.js
+++ caixa.js
@@ -9,36 +9,41 @@ process.stdin.on("data", function (valor) {
     let dez = quant.charAt(1);
     let un = quant.charAt(2);
 
-    if (quant < 100) {
-        dez = cent;
-        un = dez;
-    }
+    if (quant > 600) {
+        console.log("O valor não pode ser maior do que 600")
+    } else {
 
-    if (quant < 10) {
-        un = cent;
-    }
-
-    if (quant > 100) {
-        console.log("Você receberá " + cent + " notas de cem")
-
-        if (dez > 5) {
-            console.log("Uma nota de 50")
-            let deze = dez - 5
-            console.log(deze + " nota(s) de dez")
-        } else if (dez == 5) {
-            console.log("Uma nota de 50")
-        } else if (dez < 5 || dez > 0) {
-            console.log(dez + " nota(s) de dez")
+        if (quant < 100) {
+            dez = cent;
+            un = dez;
         }
 
-        if (un > 5) {
-            console.log("Uma nota de 5")
-            let uni = un - 5
-            console.log(uni + " nota(s) de 1")
-        } else if (un == 5) {
-            console.log("Uma nota de 5")
-        } else if (un > 0 || un < 5) {
-            console.log(un + " nota(s) de um")
+        if (quant < 10) {
+            un = cent;
+        }
+
+        if (quant > 100) {
+            console.log("Você receberá " + cent + " notas de cem")
+
+            if (dez > 5) {
+                console.log("Uma nota de 50")
+                let deze = dez - 5
+                console.log(deze + " nota(s) de dez")
+            } else if (dez == 5) {
+                console.log("Uma nota de 50")
+            } else if (dez < 5 || dez > 0) {
+                console.log(dez + " nota(s) de dez")
+            }
+
+            if (un > 5) {
+                console.log("Uma nota de 5")
+                let uni = un - 5
+                console.log(uni + " nota(s) de 1")
+            } else if (un == 5) {
+                console.log("Uma nota de 5")
+            } else if (un > 0 || un < 5) {
+                console.log(un + " nota(s) de um")
+            }
         }
     }
     process.exit();

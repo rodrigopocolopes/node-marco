74a968ba7b315d190840863095b1cc05bd2f15eb
Rodrigo Lopes <rodrigolopes.94@hotmail.com>
2025-03-14 21:20:11 -0300

    escrevendocsv

--- server.js
+++ server.js
@@ -4,11 +4,53 @@
 const express = require("express");
 const app = express();
 
-app.get("/contato", function(req, res){
-    res.send("Oi turma");
+const fs = require("fs");
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/contato", function (req, res) {
+
+    let valores = req.query;
+    res.json(valores);
+});
+
+app.post("/contato", function (req, res) {
+
+    let val = req.body.nome + "," + req.body.email + "\n";
+    
+    /* res.json(val); */
+
+    let options = {
+        flag: "a",
+    }
+
+    fs.writeFile("alunos.csv", val, options, function(){
+        
+        res.send("Email cadastrado")
+
+    })
+
+
+});
+
+app.post("/calc", function (req, res) {
+
+    let resultado = 0
+    resultado = parseInt(req.body.num1) + parseInt(req.body.num2)
+    res.send("Resultado: " + resultado)
+});
+
+app.get("/", function (req, res) {
+
+    res.send("Enviar email");
 })
 
-app.listen(3000, function(erro){
+app.listen(3000, function (erro) {
+
     console.log(erro);
     console.log("Servidor iniciado");
-})
+});
+

da865119dec869994a7370a93bae7704ebfd71bb
Rodrigo Lopes <rodrigolopes.94@hotmail.com>
2025-03-17 21:34:13 -0300

    crud

---
+++ crud/index.js
@@ -0,0 +1,10 @@
+//CRUD
+// C -> Create -> Criar
+// R -> Read -> Ler
+// U -> Update -> Atualizar
+// D -> Delete -> Apagar
+
+// DB
+// SQL   -> Relacional -> Estruturado -> Mysql, sqlserver, oracle, postgres
+// NoSQL -> Mongodb, redis, cassandra, dinamo
+
